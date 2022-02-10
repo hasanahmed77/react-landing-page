565647a6267abd011c391835daff8ebdc531c319
Hasan Ahmed <megatron.kyuubi@gmail.com>
2022-02-11 04:54:11 +0600

    made the website responsive

--- src/components/Home/PortfolioSection/Portfolio.js
+++ src/components/Home/PortfolioSection/Portfolio.js
@@ -2,6 +2,10 @@ import React from 'react'
 import Card from '../../Card/Card'
 import './Portfolio.scss'
 import BgImage1 from '../../../img/portfolio/1.jpg'
+import BgImage2 from '../../../img/portfolio/2.jpg'
+import BgImage3 from '../../../img/portfolio/3.jpg'
+import BgImage4 from '../../../img/portfolio/4.jpg'
+import BgImage5 from '../../../img/portfolio/5.jpg'
 
 const Portfolio = () => {
     return (
@@ -9,10 +13,10 @@ const Portfolio = () => {
             <p className="preheader">OUR WORKS</p>
             <h1>Portfolio</h1>
             <div className="cards">
-                <Card img={BgImage1} showInfo={false} name="Mohammad Joey" body="Serial Killer and Comedian"/>
-                <Card img={BgImage1} showInfo={false} name="Dapper Ahmed" body="Politician and Comedian"/>
-                <Card img={BgImage1} showInfo={false} name="'KING'" body="King of Zoo"/>
-                <Card img={BgImage1} showInfo={false} name="Monkey Man" body="Rapper and Comedian"/>
+                <Card img={BgImage2} showInfo={false} name="Mohammad Joey" body="Serial Killer and Comedian"/>
+                <Card img={BgImage3} showInfo={false} name="Dapper Ahmed" body="Politician and Comedian"/>
+                <Card img={BgImage4} showInfo={false} name="'KING'" body="King of Zoo"/>
+                <Card img={BgImage5} showInfo={false} name="Monkey Man" body="Rapper and Comedian"/>
             </div>
         </div>
     )
--- src/components/Home/TeamSection/Team.js
+++ src/components/Home/TeamSection/Team.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import './Team.scss'
 import Card from '../../Card/Card'
-import BgImage1 from '../../../img/portfolio/1.jpg'
+import BgImage1 from '../../../img/team/1.jpg'
+import BgImage2 from '../../../img/team/2.jpg'
+import BgImage3 from '../../../img/team/3.jpg'
+import BgImage4 from '../../../img/team/4.jpg'
 
 
 const Team = () => {
@@ -11,9 +14,9 @@ const Team = () => {
             <h1>Our Team</h1>
             <div className="cards">
                 <Card img={BgImage1} showInfo={true} name="Mohammad Joey" body="Serial Killer and Comedian"/>
-                <Card img={BgImage1} showInfo={true} name="Dapper Ahmed" body="Politician and Comedian"/>
-                <Card img={BgImage1} showInfo={true} name="'KING'" body="King of Zoo"/>
-                <Card img={BgImage1} showInfo={true} name="Monkey Man" body="Rapper and Comedian"/>
+                <Card img={BgImage2} showInfo={true} name="Dapper Ahmed" body="Politician and Comedian"/>
+                <Card img={BgImage3} showInfo={true} name="'KING'" body="King of Zoo"/>
+                <Card img={BgImage4} showInfo={true} name="Monkey Man" body="Rapper and Comedian"/>
             </div>
         </div>
     )
